5108c5f7af2094c886f9cf70fbc68e97f1103a0a
OpiniIsaac <isaacopini8@gmail.com>
2024-01-19 10:45:44 +0300

    user can be updated and single user can be fetehcd

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,17 +6,57 @@ const  getUsers =  async (req, res)=>{
      res.status(200).json(users)
     }
     catch (error){
-     res.status(500).send("cant find users");
+     res.status(500).json({ message:"user cant be got ", errorMessage:error.message});
 }
 }
+
+const  getSingleUser =  async (req, res)=>{
+   try {
+      const user = await User.findOne({ _id: req.params.id });
+  
+      if(!user){
+        return res.status(404).json({error:"user not found"})
+      }
+
+      res.status(200).json(user);
+    } catch (error) {
+     res.status(500).json({ message:"user cant be found", errorMessage:error.message});
+    }
+ }
 const addUser = async (req,res)=>{
      try {
-         //const {name, email,role,department} = req.body
-          const user = new User(req.body)
+         const {name, email,role,department} = req.body;
+          const user = new User({
+               name,
+               department,
+               email,
+               role
+          })
           await user.save()
           res.status(200).json({ message:"user added as a successfully",user});
      } catch (error) {
           res.status(500).json({ message:"user cant be added", errorMessage:error.message});
      }
 } 
-module.exports = {getUsers, addUser}
+
+const updateUser = async (req, res)=>{
+     try {
+          const user = await User
+          .findOneAndUpdate(
+            { _id: req.params.id },
+            req.body,
+            { new: true }
+          );
+      if(!user){
+        res.status(404).json({Message:"user not found"})
+      }
+          res.status(200).json(user);
+     
+          
+     } catch (error) {
+          res.status(500).json({ message:"user cant be updated", errorMessage:error.message});
+     }
+}
+
+
+module.exports = {getUsers, addUser,updateUser,getSingleUser}
--- src/routes/User.js
+++ src/routes/User.js
@@ -3,6 +3,8 @@ const controller = require ('../controllers/userController')
 const router = Router();
 
 router.get('/', controller.getUsers)
+router.get('/:id', controller.getSingleUser)
 router.post('/',controller.addUser)
+router.put('/:id', controller.updateUser)
 
 module.exports = router
